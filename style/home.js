7026172d2c645eea727a3482b6782a8469921661
rifadbasic <krifad02@gmail.com>
2025-03-02 20:04:06 +0600

    commit script part

--- style/home.js
+++ style/home.js
@@ -11,5 +11,68 @@ document.getElementById("date").textContent = dateFormat;
 document.getElementById('clr-data')
 .addEventListener('click', function clearData(){
   document.getElementById("history").textContent = '';
-  alert("Do you want to delete history");
 })
+
+
+// task count
+function task(number){
+
+
+   // task count minus
+   alert("Task no. " + number + " is Completed Successfully");
+   const taskCount = document.getElementById("task-count");
+   const taskCountFormat = parseInt(taskCount.textContent);
+   let taskCountValue = taskCountFormat - 1;
+   taskCount.textContent = taskCountValue;
+
+   
+
+   // task count plus
+   const allTask = document.getElementById("all-task");
+   const allTaskFormat = parseInt(allTask.textContent);
+   let allTaskValue = allTaskFormat + 1;
+   allTask.textContent = allTaskValue;
+
+   //  task name
+   const taskName = document.getElementById("task-name-" + number);
+   const taskNameFormat = taskName.textContent;
+
+  //  time set
+   const time = new Date();
+   const timeFormat = time.toLocaleTimeString();
+
+  
+
+   // add task list
+   document.getElementById("history").innerHTML += 
+   `<p class="
+    bg-blue-50
+     p-4 
+     rounded-lg
+     mb-3 
+     text-secondary opacity-50
+     ">
+     You have Complete The Task ${taskNameFormat}  
+      at ${timeFormat}
+     </p>`
+
+
+
+     // task count
+     const length = document.getElementById("history").children.length;
+     for (let i = 1; i <= length; i++) {
+      if(length == 6){
+        alert("You have Completed All Task");
+        break;
+      }
+     }
+     
+
+    //  disable button;
+    const taskBtn = document.getElementById(number).disabled = true;
+    taskBtn.style.background = "#3752FD";
+    taskBtn.style.cursor = "not-allowed";
+    taskBtn.style.color = "white";
+    taskBtn.style.opacity = "0.5";
+}
+
